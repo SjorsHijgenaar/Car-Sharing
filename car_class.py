a0bffffd4d3bc9949403864b224015a516c6ad5c
Sjors Hijgenaar <sjorshijgenaar@gmail.com>
2022-11-08 17:06:59 +0100

    removed unused package class

--- car_class.py
+++ car_class.py
@@ -9,13 +9,7 @@ class Package:
     self.km_price = km_price
     self.free_km = free_km
 
-class Km_Package:
-  def __init__(self, kms, price) -> None:
-    self.kms = kms
-    self.price = price
-
 class Car:
-
   def __init__(self, platform:str, model:str, make:str, hourly_rate:int, km_price:float, free_km:int):
     self.platform = platform
     self.model = model
