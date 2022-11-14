df10381273d801087c756c32b45b5febbaf3eec6
Sjors Hijgenaar <sjorshijgenaar@gmail.com>
2022-11-14 11:19:54 +0100

    first heuristic version

--- car_sharing.py
+++ car_sharing.py
@@ -1,10 +1,7 @@
-from car_class import Car
+from car_class import Package, Car
 import heapq
 
 def main():
-  distance = input("Type desired distance in kilometers: ")
-  duration = input("Type desired duration in hours: ")
-
   sixt_bmw_i3 = Car('SIXT', 'BMW', 'i3', 0.27*60, 0.39, 200)
   sixt_bmw_i3.add_package(3, 39, 0.39, 80)
   sixt_bmw_i3.add_package(6, 55, 0.39, 120)
@@ -18,10 +15,44 @@ def main():
   sixt_bmw_i3.add_package(0, 63, 0.39, 250)
   sixt_bmw_i3.add_package(0, 125, 0.39, 500)
 
-  def find_best_package_combination(car: Car, distance, duraction, price):
+  def find_best_package_combination(car: Car, distance, duration):
+    car.add_package(duration, duration * car.hourly_rate + max(0, distance - car.free_km) * car.km_price, car.km_price, car.free_km)
     # node is defined by a tuple of (cost, (distance, duration) and counts)
     # where counts is a breakdown of the number per package
-    heap = [(0, (0, 0), tuple(package, 0)) for package in car.packages]
+    heap = [(0, (0, 0), tuple((id(package), package, 0) for package in car.packages))]
+    visited = set()
+
+    while heap:
+      cost, dis_dur, counts = heapq.heappop(heap)
+      if (cost, dis_dur, counts) in visited:
+        continue
+      visited.add((cost, dis_dur, counts))
+
+      if dis_dur[0] >= distance and dis_dur[1] >= duration:
+        yield (cost, dis_dur, counts)
+
+      remaining_km = max(0, distance - dis_dur[0])
+      remaining_hrs = max(0, duration - dis_dur[1])
+      minute_rate_costs = remaining_km * car.km_price + remaining_hrs * car.hourly_rate
+
+      for _id, package, count in counts:
+        # heuristic for finding and adding package
+        if remaining_hrs == 0 and remaining_km == 0:
+          continue
+        # if 0 < minute_rate_costs < package.price:
+        #   new_cost = cost + minute_rate_costs
+        #   heapq.heappush(heap, (new_cost, (dis_dur[0] + remaining_km, dis_dur[1] + remaining_hrs), counts))
+        if 0 < package.duration < duration or remaining_hrs == 0 or duration == remaining_hrs and package.duration > 0:        
+            new_cost = cost + package.price
+            new_counts = tuple((id(p), p, c + 1 if p == package else c) for i, p, c in counts)
+            heapq.heappush(heap, (new_cost, (dis_dur[0] + package.free_km, dis_dur[1] + package.duration), new_counts))
+          
+  # distance = input("Type desired distance in kilometers: ")
+  distance = 800
+  # duration = input("Type desired duration in hours: ")
+  duration = 168
+  for best in find_best_package_combination(sixt_bmw_i3, distance, duration):
+    print(best) 
 
 if __name__ == "__main__":
     main()
